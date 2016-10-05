a50f9dd331ddf2610ef56ac63f7754a0b377ef26
areex2 <aree91@cnu.ac.kr>
2016-10-05 20:44:30 +0900

    function filtering by multiprocessing

--- similarityAnalyzer.py
+++ similarityAnalyzer.py
@@ -40,7 +40,7 @@ def filtering(queue, result_filename):
     print "Filtering time", str(time.time() - start)
 
 def isCandidate(f1, f2):
-    
+
     def filterByFunctionName(f1, f2):
         if f1['name'] == f2['name'] and f1['name'].find("sub") < 0:
             return True
