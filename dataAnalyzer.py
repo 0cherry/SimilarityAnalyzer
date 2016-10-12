fc7ac7fb635bb6070eef6264ed9b1646197f2bbd
areex2 <aree91@cnu.ac.kr>
2016-10-12 20:34:11 +0900

    modify data analysis method

--- dataAnalyzer.py
+++ dataAnalyzer.py
@@ -1,55 +1,31 @@
 import pandas
 import matplotlib.pyplot as plot
 
-# def filtering(row, cosine, lcs):
-#     def filterByFunctionName(row):
-#         if row['srcName'] == row['dstName'] and row['srcName'].find("sub") < 0:
-#             return 1
-#         return 0
-#     def filterByFunctionSize(row, threshold):
-#         f1size, f2size = row['srcNumOfMne'], row['dstNumOfMne']
-#         minsize, maxsize = min(f1size, f2size), max(f1size, f2size)
-#         standard = minsize * threshold
-#         if minsize < 5 or maxsize < 5:
-#             return 0
-#         if maxsize <= standard:
-#             return 1
-#         return 0
-#     def filterByCosine(row, threshold):
-#         if row['cosine'] >= threshold:
-#             return 1
-#         return 0
-#     def filterByMnemonicLCS(row, threshold):
-#         if row['mLCS'] >= threshold:
-#             return 1
-#         return 0
-#
-#     print row[row.srcName]
-#     filterByFunctionName(data) and filterByFunctionSize(data, 1.1) and filterByCosine(data, cosine) and filterByMnemonicLCS(data, lcs)
-
 def readFile(filepath):
     # data [srcName, srcAddr, srcNumOfMne, dstName, dstAddr, cosine, cosineTime, mLCS, mLCSTime]
     data = pandas.read_csv(filepath)
     return data
 
-def filterData(data, cosine, lcs):
+def classifyData(data, cosine, lcs):
     # noFiltered_data = data
     FilteredData = data[(data.cosine >= cosine) & (data.mLCS >= lcs)]
     NotFilteredData = data[((data.cosine < cosine) | (data.mLCS < lcs))]
 
+    print FilteredData.srcName.count(), NotFilteredData.srcName.count()
     true_positive = FilteredData[(FilteredData.srcName == FilteredData.dstName)]
-    true_negative = FilteredData[(FilteredData.srcName != FilteredData.dstName)]
+    false_positive = FilteredData[(FilteredData.srcName != FilteredData.dstName)]
     false_negative = NotFilteredData[(NotFilteredData.srcName == NotFilteredData.dstName)]
-    false_positive = NotFilteredData[(NotFilteredData.srcName != NotFilteredData.dstName)]
+    true_negative = NotFilteredData[(NotFilteredData.srcName != NotFilteredData.dstName)]
     result = [true_positive, true_negative, false_negative, false_positive]
     return result
 
-def makeGraphByFilteredData(classifiedData, cosine, lcs):
+def makeGraphByClassifiedData(classifiedData, cosine, lcs):
     true_positive = classifiedData[0].srcName.count()
     true_negative = classifiedData[1].srcName.count()
     false_negative = classifiedData[2].srcName.count()
     false_positive= classifiedData[3].srcName.count()
 
+    print true_positive, true_negative, false_negative, false_positive
     title = 'cosine {} lcs {}'.format(cosine, lcs)
     labels = ['true positive', 'true negative', 'false negative', 'false positive']
     ratio = [true_positive, true_negative, false_positive, false_negative]
@@ -65,9 +41,9 @@ def run():
     data = readFile(filepath)
 
     cosine, lcs = 0.7, 0.7
-    result = filterData(data, cosine, lcs)
+    classifiedData = classifyData(data, cosine, lcs)
 
-    makeGraphByFilteredData(result, cosine, lcs)
+    makeGraphByClassifiedData(classifiedData, cosine, lcs)
     showGraph()
 
 if __name__ == '__main__':
